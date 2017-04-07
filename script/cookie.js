e8ef2bd301b5a637a1c30f0b7901743e7e7ec634
Waldemar Kornilov <moredesign@live.de>
2017-04-07 22:34:33 +0200

    Hintergrund von "Fade" auf "Einfarbig" geändert

---
+++ script/cookie.js
@@ -0,0 +1,5 @@
+window.cookieconsent_options = {
+  message: 'Diese Website nutzt Cookies, um bestmögliche Funktionalität bieten zu können.',
+  dismiss: 'Ok, verstanden',
+  theme: 'dark-bottom';
+}
